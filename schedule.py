cb96deb0b2344a181c80ff6707e88de96658297a
Corey Alexander <coreyja@gmail.com>
2014-10-27 22:38:42 -0400

    Changed scheduler to use virtualenv python

--- schedule.py
+++ schedule.py
@@ -2,8 +2,8 @@ from plan import Plan
 
 cron = Plan()
 
-cron.command('python ~/PHTT/linkedin.py', every='1.day', at='12:00')
-cron.command('python ~/PHTT/stackoverflow.py', every='1.day', at='12:05')
+cron.command('~/Envs/PHTT/bin/python ~/PHTT/linkedin.py', every='1.day', at='12:00')
+cron.command('~/Envs/PHTT/bin/python ~/PHTT/stackoverflow.py', every='1.day', at='12:05')
 
 
 if __name__ == '__main__':
