f22a726d7f1f744890c4263b03b423fb0bed6451
Corey Alexander <coreyja@gmail.com>
2014-11-10 22:31:00 -0500

    Technologies now pull top 1000 tags from stackoverflow

--- tech.py
+++ tech.py
@@ -1,7 +1,24 @@
-
+import requests
 
 def listTechnologies():
 
-	techs = ['java', 'javascript', 'android', 'ios', 'hadoop', 'c#']
+	base_url = 'https://api.stackexchange.com'
+	tag_url = base_url + "/2.2/tags"
+
+	techs = []
+
+	for i in [x+1 for x in range(10)]:
+		payload = {
+			'order': 'desc', #Return the quantity of matching items, not the actual items
+			'sort': 'popular',
+			'site': 'stackoverflow',
+			'page': i,
+			'pagesize': 100
+		}
+
+		r = requests.get(tag_url, params=payload)
+		techs += [str(x['name']) for x in r.json()['items']]
+	
 
 	return techs
+
