8ff3dfac1502b904b648d1d89895eab1033def2b
Corey Alexander <coreyja@phtt.csse.rose-hulman.edu>
2015-01-27 21:41:38 -0500

    Added SMA to polyfit

--- get_poly_sma.py
+++ get_poly_sma.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-from datetime import date, timedelta
+from datetime import datetime, timedelta
 import couchdb
 
 import matplotlib as mpl
@@ -11,14 +11,20 @@ import matplotlib.pyplot as plt
 
 def getPolyfit(x,y, degree=3):
 	z = np.polyfit(x,y, degree)
-        p = np.poly1d(z)
+    	p = np.poly1d(z)
 
-        yhat = p(x)
-        ybar = np.sum(y)/len(y)
-        ssreg = np.sum((yhat-ybar)**2)
-        sstot = np.sum((y - ybar)**2)
+    	yhat = p(x)
+    	ybar = np.sum(y)/len(y)
+    	ssreg = np.sum((yhat-ybar)**2)
+    	sstot = np.sum((y - ybar)**2)
 
 	return (p, ssreg/sstot)
+
+def moving_average(a, n=3):
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
 
 db_name = "stackoverflow_new"
 
@@ -26,33 +32,45 @@ server = couchdb.Server()
 
 db = server[db_name]
 
-java = []
-
+# Preprocess the data. Put it in a easier format to use
 data = {}
-
 for doc in db:
 	doc_obj = db[doc]
 	for tech in doc_obj['items']:
 		name = tech['name']
 		if name not in data:
 			data[name] = []
-		data[name].append( (doc_obj['date'], tech['count']))
+		date = datetime.strptime(doc_obj['date'], "%Y-%m-%d")
+		data[name].append( (date, tech['count']))
 
 regressions = {}
 R_squared = {}
 count = 0
 numPlots = 10
-degree = 6
+degree = 3
+num_to_avg=3
 for tech in data:
 	d = data[tech]
 	d.sort()
-	y = np.array([x[1] for x in d])
-	x = np.array(range(len(y)))
 	
-	p, r_sq = getPolyfit(x,y,degree)
+	first_day = d[0][0]	
+
+	y = np.array([x[1] for x in d])
+	x = np.array([(x[0]-first_day).days for x in d])
+
+	
+	y_sma = moving_average(y, num_to_avg)
+	x_sma = x[1:-1]
+
+	p, r_sq = getPolyfit(x_sma,y_sma,degree)
 	regressions[tech] = p
-	R_squared[tech] = r_sq
-	# print R_squared[tech]
+
+	yhat = p(x_sma)
+        ybar = np.sum(y[1:-1])/len(y[1:-1])
+        ssreg = np.sum((yhat-ybar)**2)
+        sstot = np.sum((y[1:-1] - ybar)**2)
+
+	R_squared[tech] = ssreg/sstot
 
 	if count < numPlots and sum(p.coeffs) > 0:
 		xp = np.linspace(0, 60,61)
@@ -87,3 +105,4 @@ first_deriv.sort(reverse=True)
 print 'Top 10 Techs by first Deriv:'
 for d, tech in first_deriv[:10]:
 	print '%s: %f' % (tech, d) 
+
