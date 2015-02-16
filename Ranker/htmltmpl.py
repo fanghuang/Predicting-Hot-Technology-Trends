4de38f0a081033dae039b3911512878b912cf637
Fang Huang <fanghuang1992@gmail.com>
2015-02-16 06:33:59 -0500

    using jinja2 generate template webpage base on the scoring system's result, top and bottom specified number's trends

---
+++ Ranker/htmltmpl.py
@@ -0,0 +1,40 @@
+from jinja2 import Template
+
+def pageGenerator(result, num):
+
+	toplist = result[:num]
+	bottomlist = result[::-1][:num]
+	print toplist
+	print bottomlist
+	tmpl = Template(u'''\
+	<!DOCTYPE html>
+	<html>
+		<head>
+		<title>{{ variable|escape }}</title>
+		</head>
+		<body>
+			<h1>Top {{ number|escape }}</h1>
+			{%- for item in top %}
+			<img src="/pic/{{ item[0].replace('.','') }}.png"></img>
+			{%- endfor %}
+
+			<h1>Bottom {{ number|escape }}</h1>
+			{%- for item in bottom %}
+			<img src="/pic/{{ item[0].replace('.','') }}.png"></img>
+			{%- endfor %}
+		</body>
+	</html>
+	''')
+
+	html = tmpl.render(
+		variable = 'SMA Top & Bottom '+str(num)+' Trends',
+		number = num,
+		top = toplist,
+		bottom = bottomlist
+	)
+	print html
+	file = open("index.html", "w")
+
+	file.write(html)
+
+	file.close()
--- Ranker/test.py
+++ Ranker/test.py
@@ -1,9 +1,12 @@
 import ranker
 import scorers
 import importers
+import htmltmpl
 
 data = importers.mongoDBImporterFangSchema("questions")
 r = ranker.Ranker(data)
 
 # print r.testScore(scorers.countTrendsTimesPercentIncrease)
 print r.rank(scorers.countTrendsTimesPercentIncrease)
+data = r.rank(scorers.countTrendsTimesPercentIncrease)
+htmltmpl.pageGenerator(data,5)
