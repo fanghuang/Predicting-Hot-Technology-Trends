daa58235deb2f26a63a6351d7259e9c5a33bbbb3
Fang Huang <fanghuang1992@gmail.com>
2015-02-18 06:11:25 -0500

    expo generator generate the expo page

---
+++ Ranker/expo.py
@@ -0,0 +1,27 @@
+from jinja2 import Environment, FileSystemLoader
+from jinja2 import Template
+
+def expoGenerator(result, num):
+
+	length = len(result)
+	toplist = result[:num]
+	bottomlist = result[::-1][:num]
+	toplist = [(toplist[x-1][0],x) for x in range(1,num+1)]
+	bottomlist = [(bottomlist[x][0],length-x) for x in range(0,num)]
+
+	env = Environment(loader=FileSystemLoader('Template'))
+	template = env.get_template('/expotmpl.html')
+	html = template.render(
+		variable = 'SMA Top & Bottom '+str(num)+' Technology Trends',
+		total = length,
+		top = toplist,
+		bottom = bottomlist
+		)
+
+	print html
+
+	file = open("Template/expo.html", "w")
+
+	file.write(html)
+
+	file.close()
