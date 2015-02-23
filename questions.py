dd38fb03964e06c3ad34b89d06f416b821f5a5c3
Fang Huang <fanghuang1992@gmail.com>
2015-02-23 18:12:22 -0500

    request last 5 years

--- questions.py
+++ questions.py
@@ -5,21 +5,20 @@ from dateutil.relativedelta import relativedelta
 
 url = 'https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow&filter=!9YdnSQVoS'
 
-firstdate = date(2011, 7, 1)
+firstdate = date(2010, 4, 1)
 epoch = date(1970,1,1)
 
 server = couchdb.Server()
 #db = server.create('questions')
 tagdb = server['tags']
-tags = tagdb['a2fa8c775de9bc7d97ea22c5a9135649']['items']
+tags = tagdb['a2fa8c775de9bc7d97ea22c5a9159246']['items']
 
-db = server['questions']
+db = server['questions_2000']
 #tags = ['javascript','java','c','python','swift']
 
-for i in range(18):
-#for i in range(1):
-	t1 = firstdate - relativedelta(months=i+1)
-	t2 = firstdate - relativedelta(months=i)
+for i in range(5):
+	t1 = firstdate + relativedelta(months=i)
+	t2 = firstdate + relativedelta(months=i+1)
 	d1 = int(((t1) - epoch).total_seconds())
 	d2 = int(((t2) - epoch).total_seconds())
 	time_series = [d1, d2]
