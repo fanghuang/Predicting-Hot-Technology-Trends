b6329b5c004c8c92f424ab2b6f83af37ae9efb50
Corey Alexander <coreyja@gmail.com>
2015-03-24 22:41:07 -0400

    Added Vagrant and Ansible configurations

--- schedule.py
+++ schedule.py
@@ -2,8 +2,7 @@ from plan import Plan
 
 cron = Plan()
 
-cron.command('~/Envs/PHTT/bin/python ~/PHTT/linkedin.py', every='1.day', at='12:00')
-cron.command('~/Envs/PHTT/bin/python ~/PHTT/stackoverflow.py', every='1.day', at='12:05')
+cron.command('python /vagrant/stackoverflow.py', every='1.day', at='12:05')
 
 
 if __name__ == '__main__':
