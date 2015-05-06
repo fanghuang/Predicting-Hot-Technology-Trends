4d72e498388e9e23107c450238cc9e0444c1c27f
Corey Alexander <coreyja@gmail.com>
2015-05-06 12:50:06 -0400

    Added Email Notification feature and crom job

--- schedule.py
+++ schedule.py
@@ -3,6 +3,7 @@ from plan import Plan
 cron = Plan()
 
 cron.command('python /vagrant/stackoverflow.py', every='1.day', at='12:05')
+cron.command('python /vagrant/sendEmail.py', every='1.week')
 
 
 if __name__ == '__main__':
